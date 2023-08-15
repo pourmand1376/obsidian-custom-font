ddf9ef2d65ea9da414c4d04050c861268faac8b1
Amir Pourmand <pourmand1376@gmail.com>
2023-08-15 15:39:20 +0000

    add initial commit

--- main.ts
+++ main.ts
@@ -2,68 +2,73 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface FontPluginSettings {
 	mySetting: string;
-}
+	font: string; 
+  }
+  
+  const DEFAULT_SETTINGS: FontPluginSettings = {
+	mySetting: 'default',
+	font: 'Shabnam'
+  }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+async function downloadFont(fontName:string) {
+
+	new Notice(`Downloading ${fontName} font...`);
+  
+	// Download logic here
+  
+	const fontCSS = //...
+  
+	new Notice('Font downloaded successfully!');
+  
+	return fontCSS;
+  
+  }
+
+  function applyCss(css) {
+
+	// Create style tag
+	const style = document.createElement('style'); 
+	
+	// Add CSS content
+	style.innerHTML = css;
+  
+	// Append style tag to head
+	document.head.appendChild(style);
+  
+	// Optional: Remove existing custom CSS
+	const existingStyle = document.getElementById('farsi-font-plugin-css');
+	if (existingStyle) {
+	  existingStyle.remove(); 
+	}
+  
+	// Give ID to new style tag
+	style.id = 'farsi-font-plugin-css';
+  
+  }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: FontPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		const font = this.settings.font;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		if (font === 'Shabnam') {
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+			const css = await downloadFont('Shabnam');
+			
+			applyCss(css);
+		  
+		  } else if (font === 'Vazirmatn') {
+		  
+			const css = await downloadFont('Vazirmatn');
+			
+			applyCss(css);
+			
+		  }
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
@@ -91,21 +96,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
@@ -121,14 +112,18 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		.setName('Font')
+		.setDesc('Select the font for the Obsidian workspace')
+		.addDropdown(dropdown => {
+			dropdown.addOptions({
+			'Shabnam': 'Shabnam',
+			'Vazirmatn': 'Vazirmatn'
+			})
+			.setValue(this.plugin.settings.font)
+			.onChange(async (value) => {
+			this.plugin.settings.font = value;
+			await this.plugin.saveSettings();
+			});
+		})
 	}
 }
