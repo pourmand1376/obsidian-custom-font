09fe045d9b4ed919e536c91b17950e70a2dab6dd
Amir Pourmand <pourmand1376@gmail.com>
2023-08-16 06:04:26 +0000

    add sample commands to get release

---
+++ get_release.sh
@@ -0,0 +1,5 @@
+# developer commands to get new releases directly from github and install it in obsidian repo
+rm mainfest.json
+rm main.js
+wget https://github.com/pourmand1376/obsidian-custom-font/releases/download/v1.0.1/main.js
+wget https://github.com/pourmand1376/obsidian-custom-font/releases/download/v1.0.1/manifest.json
