e65e88712868b8440f04bf9bb06ae679edfb2129
Amir Pourmand <pourmand1376@gmail.com>
2023-09-23 10:53:11 +0000

    Add Alpha

--- version-bump.mjs
+++ version-bump.mjs
@@ -2,6 +2,13 @@ import { readFileSync, writeFileSync } from "fs";
 
 const targetVersion = process.env.npm_package_version;
 
+if (targetVersion.includes("alpha")) {
+    
+    let manifest = JSON.parse(readFileSync("manifest-beta.json", "utf8"));
+    manifest.version = targetVersion;
+    writeFileSync("manifest-beta.json", JSON.stringify(manifest, null, "\t"));
+}
+else {
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
@@ -12,3 +19,5 @@ writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
 writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+}
+
